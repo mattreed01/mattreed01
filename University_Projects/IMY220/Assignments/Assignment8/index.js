48a4a1adaa9bc67db47a198d22dd1539bdc01dfd
mattreed01 <92081436+mattreed01@users.noreply.github.com>
2021-10-07 11:29:28 +0200

    Update index.js

--- University_Projects/IMY220/Assignments/Assignment8/index.js
+++ University_Projects/IMY220/Assignments/Assignment8/index.js
@@ -1,1 +1,12 @@
 
+var modules = [
+	{code: "IMY220", department: "Information Science", mark: 74},
+	{code: "COS212", department: "Computer Science", mark: 48},
+	{code: "IMY310", department: "Information Science", mark: 78},
+	{code: "WTW126", department: "Mathematics", mark: 45},
+	{code: "AIM101", department: "Information Science", mark: 88},
+	{code: "COS110", department: "Computer Science", mark: 68},
+	{code: "VIO102", department: "Visual Arts", mark: 80},
+	{code: "COS330", department: "Computer Science", mark: 75},
+	{code: "IMY320", department: "Information Science", mark: 62},
+];
